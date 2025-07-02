129d77ff6e22d059c269790d67997dc85cdb260d
DhruvinUnikon <dhruvin@unikon.ai>
2025-07-02 22:24:23 +0530

    fix: middleware issue

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,17 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    'redis': {
+      enabled: true,
+      config: {
+        connections: {
+          default: {
+            connection: {
+              host: env('REDIS_HOST'),
+              port: env('REDIS_PORT'),
+              password: env('REDIS_PASSWORD'),
+              db: 0,
+            },
+          },
+        },
+      },
+    },
+  });
